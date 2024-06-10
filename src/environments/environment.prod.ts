33d87fccc627baf71ec444e9477b998f980b2ce3
Washington <washovilla78@gmail.com>
2024-06-10 15:35:31 -0500

    Finalización del proyecto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDRlCLPvFPGyQ0_cYBzV374ehsT38ciZGM",
+    authDomain: "geolocalizacion-533fc.firebaseapp.com",
+    databaseURL: "https://geolocalizacion-533fc-default-rtdb.firebaseio.com",
+    projectId: "geolocalizacion-533fc",
+    storageBucket: "geolocalizacion-533fc.appspot.com",
+    messagingSenderId: "1035865509278",
+    appId: "1:1035865509278:web:f52fa02282fd1a6abc9a8f",
+    measurementId: "G-J7D3S2N1KM"
+    }
 };
